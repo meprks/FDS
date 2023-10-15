b7bfd19840d28a5accabb4b19b10595c03ecf5a3
Prakash KC <143744422+meprks@users.noreply.github.com>
2023-10-15 12:52:08 -0400

    Add files via upload

--- project.py
+++ project.py
@@ -1,19 +1,18 @@
 import streamlit as st
-import pandas as pd
+import pandas as pd 
 import seaborn as sns
 import matplotlib.pyplot as plt
 import numpy as np
 from sklearn.metrics import mean_absolute_error, mean_squared_error
 
-c1, c2 = st.columns(2)
+c1,c2=st.columns(2)
 c1.markdown("# Hello, I am Prakash KC")
 c2.markdown("# This is my Webapp!")
 
 # Load your dataset
 df = pd.read_csv("Sleep_health_and_lifestyle_dataset.csv")
 
-# Exclude categorical columns
-categorical_columns = ["gender", "occupation", "sleep_disorder", "bmi_category", "blood_pressure"]
+categorical_columns = ["Gender", "Occupation", "Sleep Disorder", "BMI Category", "Blood Pressure"]
 numerical_columns = [col for col in df.columns if col not in categorical_columns]
 
 # Display the title
@@ -23,9 +22,9 @@ st.write("## Dataset for Sleep_health_and_lifestyle")
 st.dataframe(df)
 
 plot_type = st.selectbox(
-    "Select a Plot",
+    "Select a Plot", 
     (
-        "Scatter",
+        "Scatter", 
         "Relational Plot",
         "Categorical Plot",
         "Line Plot",
@@ -38,57 +37,58 @@ plot_type = st.selectbox(
 if plot_type == 'Linear Regression':
     x = st.selectbox('x_axis:', numerical_columns)
     y = st.selectbox('y_axis', numerical_columns)
-
+    
     slope = st.slider('Slope', min_value=-10.0, max_value=10.0, value=0.0)
     intercept = st.slider('Intercept', min_value=-10.0, max_value=10.0, value=0.0)
-
+    
     fig, ax = plt.subplots()
     sns.scatterplot(data=df, x=x, y=y, ax=ax)
-
+    
     x_values = np.array(df[x])
     y_values = slope * x_values + intercept
-
+    
     sns.lineplot(x=x_values, y=y_values, color='red', ax=ax)
-
+    
     y_actual = np.array(df[y])
     mae = mean_absolute_error(y_actual, y_values)
     mse = mean_squared_error(y_actual, y_values)
-
+    
     st.write(f"Mean Absolute Error (MAE): {mae}")
     st.write(f"Mean Squared Error (MSE): {mse}")
-
+    
 elif plot_type == 'Scatter':
-    x = st.selectbox('x_axis:', numerical_columns)
-    y = st.selectbox('y_axis', numerical_columns)
-
+    x = st.selectbox('x_axis:', df.columns)
+    y = st.selectbox('y_axis', df.columns)
+    
     sns.scatterplot(data=df, x=x, y=y)
-
+    
 elif plot_type == 'Relational Plot':
-    x = st.selectbox('x_axis:', numerical_columns)
-    y = st.selectbox('y_axis', numerical_columns)
-
+    x = st.selectbox('x_axis:', df.columns)
+    y = st.selectbox('y_axis', df.columns)
+    
     sns.scatterplot(data=df, x=x, y=y)
 
 elif plot_type == 'Categorical Plot':
-    x = st.selectbox('Select data you want to plot', numerical_columns)
-
-    sns.histplot(data=df, x=x)
+    x = st.selectbox('Select data you want to plot', df.columns)
+    
+    sns.histplot(data=df, x=x)    
 
 elif plot_type == 'Line Plot':
-    x = st.selectbox('x_axis:', numerical_columns)
-    y = st.selectbox('y_axis', numerical_columns)
-
+    x = st.selectbox('x_axis:', df.columns)
+    y = st.selectbox('y_axis', df.columns)
+    
     sns.lineplot(data=df, x=x, y=y)
-
+    
 elif plot_type == 'Bar Plot':
-    x = st.selectbox('x_axis:', numerical_columns)
-    y = st.selectbox('y_axis', numerical_columns)
-
+    x = st.selectbox('x_axis:', df.columns)
+    y = st.selectbox('y_axis', df.columns)
+    
     sns.barplot(data=df, x=x, y=y)
+    
 
 elif plot_type == 'Heatmap':
     # Assuming you want a heatmap of correlations between numerical columns
-    corr_matrix = df[numerical_columns].corr()
+    corr_matrix = df.corr()
     sns.heatmap(corr_matrix, annot=True, cmap='coolwarm')
-
+    
 st.pyplot(plt.gcf())
