1bbb8fe65ae9bd78987b4b7f0e46ae84ee8700f7
Prakash KC <143744422+meprks@users.noreply.github.com>
2023-10-17 15:10:15 -0400

    Update story2.py

--- story2.py
+++ story2.py
@@ -1,51 +1,36 @@
 import streamlit as st
-import pandas as pd
+import pandas as pd 
 import matplotlib.pyplot as plt
 import seaborn as sns
 
 st.set_option('deprecation.showPyplotGlobalUse', False)
+
 st.title("Exploring Sleep and Health Data")
 
-df = pd.read_csv("Sleep_health_and_lifestyle_dataset.csv")
+df = pd.read_csv("Sleep_health_and_lifestyle_dataset.csv") 
 
-show_story = st.radio("Do you want to see the story?", ("Yes", "No"))
+with st.expander("See plots"):
 
-if show_story == "Yes":
-    st.subheader("Age vs. Sleep Duration (Hue: Gender)")
-    sns.jointplot(data=df, x="Age", y="Sleep Duration", hue="Gender", kind="scatter")
-   
-    st.write("### What does the Plot show?")
-    st.write("The plot visualizes the relationship between Age and Sleep Duration, "
-               "color-coded by Gender. It shows that females with a high age group have a higher sleep duration than males. "
-               "The average sleep duration for males generally lies between 7 and 8 hours, while for females, it is between 8 and 9 hours.")
+  col1, col2 = st.columns(2)
 
-    st.subheader("Physical Activity Level vs. Sleep Duration")
-    sns.regplot(data=df, x="Physical Activity Level", y="Sleep Duration")
+  with col1:
+    st.subheader("Age vs Sleep Duration")
+    fig = sns.jointplot(data=df, x="Age", y="Sleep Duration", hue="Gender", kind="scatter")
+    st.pyplot(fig)
+
+    st.subheader("Physical Activity vs Sleep Duration")
+    fig2 = sns.regplot(data=df, x="Physical Activity Level", y="Sleep Duration")
+    st.pyplot(fig2)
+
+  with col2:
+    st.subheader("Stress Level vs Sleep Duration")
+    fig3 = sns.boxplot(data=df, x="Stress Level", y="Sleep Duration") 
+    st.pyplot(fig3)
+
+    st.subheader("BMI Category vs Sleep Duration")
+    fig4 = sns.boxplot(data=df, x="BMI Category", y="Sleep Duration")
+    st.pyplot(fig4)
     
-    st.write("### What does the Plot show?")
-    st.write("The plot visualizes the relationship between Physical Activity Level and Sleep Duration. "
-               "It shows a positive correlation, indicating that people with high physical activity levels tend to have longer sleep durations.")
-    
-    st.subheader("Stress Level vs. Sleep Duration")
-    sns.boxplot(data=df, x="Stress Level", y="Sleep Duration")
-    
-    st.write("### What does the Plot show?")
-    st.write("The plot visualizes the relationship between Stress Level and Sleep Duration. "
-               "It shows that higher stress levels are associated with longer sleep durations, suggesting that stress may affect sleep duration positively.")
-
-    st.subheader("BMI Category vs. Sleep Duration")
-    sns.boxplot(data=df, x="BMI Category", y="Sleep Duration")
-
-    st.write("### What does the Plot show?")
-    st.write("The plot visualizes the relationship between BMI Category and Sleep Duration. "
-               "It shows that individuals with normal weight tend to have better sleep duration compared to those who are overweight or obese.")
-
-    st.subheader("Sleep Disorder vs. Sleep Duration")
-    sns.boxplot(data=df, x="Sleep Disorder", y="Sleep Duration")
-    
-    st.write("### What does the Plot show?")
-    st.write("The plot visualizes the relationship between Sleep Disorder and Sleep Duration. "
-               "It shows that individuals with insomnia tend to have shorter sleep durations.")
-
-
-st.pyplot(plt.gcf())
+    st.subheader("Sleep Disorder vs Sleep Duration")
+    fig5 = sns.boxplot(data=df, x="Sleep Disorder", y="Sleep Duration")
+    st.pyplot(fig5)
