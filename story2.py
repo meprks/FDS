54184137b250b21ec8c57dac8a1e259eed5e881b
Prakash KC <143744422+meprks@users.noreply.github.com>
2023-10-17 16:04:38 -0400

    Update story2.py

--- story2.py
+++ story2.py
@@ -15,23 +15,23 @@ if show_story == "Yes":
     scatter_plot = alt.Chart(df).mark_circle().encode(
         x='Age',
         y='Sleep Duration',
-        color='Gender',
+        color=alt.Color('Gender', scale=alt.Scale(scheme='dark2')),
         tooltip=['Age', 'Sleep Duration', 'Gender']
     )
     st.altair_chart(scatter_plot, use_container_width=True)
 
     st.subheader("Physical Activity Level vs. Sleep Duration")
-    scatter_plot = px.scatter(df, x="Physical Activity Level", y="Sleep Duration")
+    scatter_plot = px.scatter(df, x="Physical Activity Level", y="Sleep Duration", color_discrete_sequence=px.colors.qualitative.Dark24)
     st.plotly_chart(scatter_plot)
 
     st.subheader("Stress Level vs. Sleep Duration")
-    box_plot = px.box(df, x="Stress Level", y="Sleep Duration")
+    box_plot = px.box(df, x="Stress Level", y="Sleep Duration", color="Stress Level", color_discrete_sequence=px.colors.qualitative.Dark24)
     st.plotly_chart(box_plot)
 
     st.subheader("BMI Category vs. Sleep Duration")
-    box_plot = px.box(df, x="BMI Category", y="Sleep Duration")
+    box_plot = px.box(df, x="BMI Category", y="Sleep Duration", color="BMI Category", color_discrete_sequence=px.colors.qualitative.Dark24)
     st.plotly_chart(box_plot)
 
     st.subheader("Sleep Disorder vs. Sleep Duration")
-    box_plot = px.box(df, x="Sleep Disorder", y="Sleep Duration")
+    box_plot = px.box(df, x="Sleep Disorder", y="Sleep Duration", color="Sleep Disorder", color_discrete_sequence=px.colors.qualitative.Dark24)
     st.plotly_chart(box_plot)
