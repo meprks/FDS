33779133c845f41926b7ee58c89e6e8167a93139
Prakash KC <143744422+meprks@users.noreply.github.com>
2023-10-17 14:30:23 -0400

    Update modified.py

--- modified.py
+++ modified.py
@@ -161,21 +161,21 @@ if show_story == "Yes":
     
     st.subheader("Stress Level vs. Sleep Duration")
     sns.boxplot(data=df, x="Stress Level", y="Sleep Duration")
-    st.pyplot()
+    st.pyplot(plt.gcf())
     st.write("### What does the Plot show?")
     st.write("The plot visualizes the relationship between Stress Level and Sleep Duration. "
                "It shows that higher stress levels are associated with longer sleep durations, suggesting that stress may affect sleep duration positively.")
 
     st.subheader("BMI Category vs. Sleep Duration")
     sns.boxplot(data=df, x="BMI Category", y="Sleep Duration")
-    st.pyplot()
+    st.pyplot(plt.gcf())
     st.write("### What does the Plot show?")
     st.write("The plot visualizes the relationship between BMI Category and Sleep Duration. "
                "It shows that individuals with normal weight tend to have better sleep duration compared to those who are overweight or obese.")
 
     st.subheader("Sleep Disorder vs. Sleep Duration")
     sns.boxplot(data=df, x="Sleep Disorder", y="Sleep Duration")
-    st.pyplot()
+    st.pyplot(plt.gcf())
     st.write("### What does the Plot show?")
     st.write("The plot visualizes the relationship between Sleep Disorder and Sleep Duration. "
                "It shows that individuals with insomnia tend to have shorter sleep durations.")
